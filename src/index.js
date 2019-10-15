7e27fab8d307a6148323445d279447bd37b08c84
Michael Rawlings <mirawlings@ebay.com>
2019-10-14 19:20:13 -0700

    bad formatting

--- src/index.js
+++ src/index.js
@@ -18,11 +18,7 @@ if (!process.env.BUNDLE) {
 function fixFlush() {
     try {
         var OutgoingMessage = require("http").OutgoingMessage;
-        if (
-            OutgoingMessage.prototype.flush &&
-            OutgoingMessage.prototype.flush.toString().indexOf("deprecated") !==
-                -1
-        ) {
+        if (OutgoingMessage.prototype.flush && OutgoingMessage.prototype.flush.toString().indexOf("deprecated") !== -1) {
             // Yes, we are monkey-patching http. This method should never have been added and it was introduced on
             // the iojs fork. It was quickly deprecated and I'm 99% sure no one is actually using it.
             // See:
@@ -32,8 +28,7 @@ function fixFlush() {
             // This method causes problems since marko looks for the flush method and calls it found.
             // The `res.flush()` method is introduced by the [compression](https://www.npmjs.com/package/compression)
             // middleware, but, otherwise, it should typically not exist.
-            delete require("http").OutgoingMessage.prototype.flush;
-        }
+            delete require("http").OutgoingMessage.prototype.flush;}
     } catch (e) {
         /* ignore error */
     }
